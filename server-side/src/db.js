977063db6a1d2b48744531d750a11b6890d2a88f
CODEX-17 <112467114+CODEX-17@users.noreply.github.com>
2025-01-03 10:35:57 +0800

    Update db.js

--- server-side/src/db.js
+++ server-side/src/db.js
@@ -3,7 +3,7 @@ const mysql = require('mysql')
 const pool = mysql.createPool(
     {
         connectionLimit: 10,
-        host: 'localhost',
+        host: '127.0.0.1',
         user: 'root',
         password: '',
         database: 'smart_library',
