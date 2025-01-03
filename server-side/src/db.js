17e9e7e02a53a2c15f7acf22ffb50bd654604ad2
CODEX-17 <112467114+CODEX-17@users.noreply.github.com>
2025-01-03 09:56:23 +0800

    update db info

--- server-side/src/db.js
+++ server-side/src/db.js
@@ -3,11 +3,10 @@ const mysql = require('mysql')
 const pool = mysql.createPool(
     {
         connectionLimit: 10,
-        host: 'sql12.freesqldatabase.com',
-        user: 'sql12754002',
-        password: 'xQGTf8MGVY',
-        database: 'sql12754002',
-        port: 3306,
+        host: 'localhost',
+        user: 'root',
+        password: '',
+        database: 'smart_library',
     }
     // {
     //     connectionLimit: 10,
