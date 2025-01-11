a757e810590a6915949a9549c116e56da9e764d7
CODEX-17 <112467114+CODEX-17@users.noreply.github.com>
2025-01-11 23:15:56 +0800

    change db1

--- server-side/src/db.js
+++ server-side/src/db.js
@@ -3,12 +3,20 @@ const mysql = require('mysql')
 const pool = mysql.createPool(
     {
         connectionLimit: 10,
-        host: 'sql12.freesqldatabase.com',
-        user: 'sql12756965',
-        password: '5kC76xvCzA',
-        database: 'sql12756965',
+        host: 'srv1785.hstgr.io',
+        user: 'u267839543_smartLibrary',
+        password: 'Admin123456789!!',
+        database: 'u267839543_smartLibrary12',
         port: 3306,
     }
+    // {
+    //     connectionLimit: 10,
+    //     host: 'sql12.freesqldatabase.com',
+    //     user: 'sql12756965',
+    //     password: '5kC76xvCzA',
+    //     database: 'sql12756965',
+    //     port: 3306,
+    // }
     // {
     //     connectionLimit: 10,
     //     host: '82.112.236.213',
